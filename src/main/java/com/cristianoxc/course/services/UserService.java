3bf8f24cca1c2b4bdaa6adb54bb6272a22500d80
Cristiano Xavier <cristianoxc@yahoo.com.br>
2021-05-27 20:36:50 -0300

    update user

--- src/main/java/com/cristianoxc/course/services/UserService.java
+++ src/main/java/com/cristianoxc/course/services/UserService.java
@@ -31,4 +31,19 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		// o getOne instancia o usuario sem ir no DB, vai deixar somente o objeto 
+		// monitorado no JPA para trabalhar com ele e em seguida efetuar alguma operação com DB.
+		// É diferente do findById que vai no DB e traz o objeto.
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setname(obj.getname());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
